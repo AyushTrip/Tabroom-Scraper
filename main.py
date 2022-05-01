6e1623dc640fc417218d336a78b1cbf1438533f2
Ayush Tripathi <62866081+AyushTrip@users.noreply.github.com>
2022-04-30 19:27:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,36 +30,33 @@ def find_speaks(x, speak):
 #Take URL and Round Input
 url = str(input("Enter Round 1 Tabroom URL: "))
 rounds = int(input("Enter # of Prelims: "))
+
+#Find out tournament result style
 ask_speak = str(input("Does The Tournament List Speaker Positions in the Results Category y/n: "))
-
-print("Tabulating Averages . . . ")
-
 speak_pos = False
-
-if(ask_speak == 'y'):
-  speak_pos = True
+if(ask_speak == 'y'): speak_pos = True
 
 #Generate all round links
 last_part_id = int(url[-2:])
 round_links = []
-
 temp_rounds = 0
+
 for i in range(rounds):
   round_links.append(url[0:-2]+str(last_part_id+temp_rounds))
   temp_rounds += 1
 
-#Open Links
-
+#Create Dictionaries
 final_speaks = dict()
 rounds_judged = dict()
 
-
+#Open Links
+#SCRAPING PROCESS ============================================
 try:
+  
   for i in range(len(round_links)):
 
     round_html = requests.get(round_links[i])
     round_soup = BeautifulSoup(round_html.text, 'html.parser')
-    
     dfs = pd.read_html(round_html.text)
     table = dfs[0]
 
@@ -71,51 +68,47 @@ try:
         t1 = find_speaks(str(table.iloc[j,4]), speak_pos)
         t2 = find_speaks(str(table.iloc[j,5]), speak_pos)
         judge_name = table.iloc[j,2]
-    
         average = float(t1+t2)/2
 
         #Check if judge is in the dictionary
         
         #If NO
         if judge_name not in final_speaks:
+          
           final_speaks.update({judge_name:average})
           rounds_judged.update({judge_name:1})
           
         #If YES
         else:
+          
           current_avg = final_speaks.get(judge_name)
           current_rounds = rounds_judged.get(judge_name) 
           final_speaks[judge_name] = current_avg + average
           rounds_judged[judge_name] = current_rounds + 1
           
       except:
+        
         pass
 
 except:
+  
   pass
 
-#Now calculate the average
+#Calculate Average
 for key, value in final_speaks.items():
   rounds = rounds_judged.get(key)
   final_speaks[key] = round(float(value/rounds),4)
 
-#print(json.dumps(final_speaks, indent=3))
-
-#SPREADSHEET TRANSFER
-
-#Spreedsheet Information Transfer
+#SPREADSHEET TRANSFER ===========================
 wb = load_workbook('tabroom-automation/yao.xlsx')
 ws = wb.active
 
 #Iterate through all the judges in the dictionary
-
 for key, value in final_speaks.items():
 
     value_done = False
-
     row = 2
     col = 1
-
 
     while not value_done:
 
@@ -134,11 +127,15 @@ for key, value in final_speaks.items():
               iteration += 1
               
       elif ws.cell(row=row,column=col).value is None:
+        
           ws.cell(row=row,column=col).value = str(key)
           ws.cell(row=row,column=col+1).value = value
           value_done = True
           
       else:
+        
         row += 1
 
 wb.save('tabroom-automation/yao.xlsx')
+
+#PROPERTY OF WESTWOOD DEBATE
